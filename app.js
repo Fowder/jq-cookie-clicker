95836e6063fad0fbd597e00321351158d37559be
Fowder <emile.troccaz@hotmail.com>
2017-12-07 17:00:13 +0100

    Ajout phrases

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var obj = {
 	0 : "Achete toi une vie !",
 	1 : "Arrête ça !",
-	2 : "Rentre chez toi !"
+	2 : "Rentre chez toi !",
+	3 : "Vas jouer ailleurs !",
+	4 : "Va travailler !"
 };
 
 var image = $('.cake-is-a-lie');
 function clic (){
 	image.click(function(){
-		var alea = 0+Math.floor(Math.random()*3);
+		var alea = 0+Math.floor(Math.random()*5);
 		var compter = $('#compte').text();
 		compter++;
 		$('#compte').html(compter);
