7bdb9bc0793cfe06fa1a38891e64f04235540579
Fowder <emile.troccaz@hotmail.com>
2017-12-07 16:57:10 +0100

    Résolution exo

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-$(document).ready(function(){
-	//votre code
+var obj = {
+	0 : "Achete toi une vie !",
+	1 : "Arrête ça !",
+	2 : "Rentre chez toi !"
+};
 
+var image = $('.cake-is-a-lie');
+function clic (){
+	image.click(function(){
+		var alea = 0+Math.floor(Math.random()*3);
+		var compter = $('#compte').text();
+		compter++;
+		$('#compte').html(compter);
+		var a = 20;
+		var multiple = compter % a;
+		if (multiple == 0){
+		alert(obj[alea]);
+	}
+	})
+}
+
+$(document).ready(function(){
+	
+clic();
 
 });
